e1c13f426286c8adc96ff8173097745eafae88ac
domifara <domifara@18a1db3e-636b-4d27-a0a4-934d44d94851>
2012-01-12 23:34:52 +0000

    Supported "$modversion['trust_dirname']". #245 http://www.xoopscube.net/redmine/issues/245
    git-svn-id: https://svn.sourceforge.jp/svnroot/d3pipes/d3pipes@11 18a1db3e-636b-4d27-a0a4-934d44d94851

--- xoops_trust_path/modules/d3pipes/xoops_version.php
+++ xoops_trust_path/modules/d3pipes/xoops_version.php
@@ -19,6 +19,7 @@ $modversion['license'] = "GPL" ;
 $modversion['official'] = 0 ;
 $modversion['image'] = file_exists( $mydirpath.'/module_icon.png' ) ? 'module_icon.png' : 'module_icon.php' ;
 $modversion['dirname'] = $mydirname ;
+$modversion['trust_dirname'] = $mytrustdirname ;
 $modversion['read_any'] = true ;
 
 // Any tables can't be touched by modulesadmin.
@@ -43,6 +44,15 @@ $modversion['sub'] = array() ;
 if( is_object( @$GLOBALS['xoopsModule'] ) && $GLOBALS['xoopsModule']->getVar('dirname') == $mydirname ) {
 	require_once dirname(__FILE__).'/include/common_functions.php' ;
 	$modversion['sub'] = d3pipes_common_get_submenu( $mydirname ) ;
+} else {
+	$_sub_menu_cache = XOOPS_TRUST_PATH . '/cache/'. urlencode(substr(XOOPS_URL, 7)) . '_' . $mydirname . '_' . (is_object(@$GLOBALS['xoopsUser'])? join('-', $GLOBALS['xoopsUser']->getGroups()):XOOPS_GROUP_ANONYMOUS)  . '_' . $GLOBALS['xoopsConfig']['language'] . '.submenu';
+	if (is_file($_sub_menu_cache) && time() - 3600 < filemtime($_sub_menu_cache)) {
+		$modversion['sub'] = unserialize(file_get_contents($_sub_menu_cache));
+	} else {
+		require_once dirname(__FILE__).'/include/common_functions.php' ;
+		$modversion['sub'] = d3pipes_common_get_submenu( $mydirname ) ;
+		file_put_contents($_sub_menu_cache, serialize($modversion['sub']));
+	}
 }
 
 // All Templates can't be touched by modulesadmin.
