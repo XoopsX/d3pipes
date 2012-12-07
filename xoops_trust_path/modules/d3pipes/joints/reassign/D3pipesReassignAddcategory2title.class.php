2647c70ab2ddb0914e585c53e627c6119f9386dd
Naoki okino <naao@mbg.nifty.com>
2012-12-07 21:31:59 +0900

    Added a reassign joint "D3pipesReassignAddcategory2title" which add category name to the headline

---
+++ xoops_trust_path/modules/d3pipes/joints/reassign/D3pipesReassignAddcategory2title.class.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once dirname(dirname(__FILE__)).'/D3pipesReassignAbstract.class.php' ;
+
+class D3pipesReassignAddcategory2title extends D3pipesReassignAbstract {
+
+	function execute( $entries , $max_entries = 10 )
+	{
+		foreach( array_keys( $entries ) as $i ) {
+			$entry =& $entries[ $i ] ;
+			if( ! empty( $entry['category'] )) {
+				$entry['headline'] = '['.$entry['category'].'] '.$entry['headline'] ;
+			}
+		}
+	
+		return $entries ;
+	}
+
+	function renderOptions( $index , $current_value = null )
+	{
+		$index = intval( $index ) ;
+
+		return '<input type="hidden" name="joint_option['.$index.']" id="joint_option_'.$index.'" value="'.htmlspecialchars($current_value,ENT_QUOTES).'" />' ;
+	}
+}
+
+?>
