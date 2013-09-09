6a0564031f06c1b4156a3750ad8d185b8e486486
nao-pon <hypweb@gmail.com>
2013-09-09 17:58:07 +0900

    commit miss revert

--- xoops_trust_path/modules/d3pipes/xoops_version.php
+++ xoops_trust_path/modules/d3pipes/xoops_version.php
@@ -11,7 +11,7 @@ $constpref = '_MI_' . strtoupper( $mydirname ) ;
 
 $modversion['name'] = $mydirname ;
 $modversion['description'] = constant($constpref.'_DESC') ;
-$modversion['version'] = 0.70 ;
+$modversion['version'] = 0.69 ;
 $modversion['credits'] = "PEAK Corp.";
 $modversion['author'] = "GIJ=CHECKMATE<br />PEAK Corp.(http://www.peak.ne.jp/)" ;
 $modversion['help'] = "" ;
